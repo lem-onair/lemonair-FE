100df0ed85b2e31b73113e8e9a35f24eb5ecb4c0
BYEONGRYEOL <lyulbyung@gmail.com>
2023-12-19 10:02:05 +0900

    채널리스트받아오기

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ import styled from "styled-components";
 // import SquareButton from "./styled_components/SquredButton";
 // import RotateBox from "./animated_components/RotateButton";
 // import BlinkEventButton from "./event_components/BlinkEventButton";
-import ChannelInfo from "./sbltest/Channel";
+import Channels from "./sbltest/ChannelList";
+import Channel from "./sbltest/Channel";
 import VideoPlayer from "./ReactPlayer";
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const StBox = styled.div`
   width: 100px;
@@ -16,9 +19,19 @@ const StBox = styled.div`
 
 const App = () => {
   return (
-    <div>
-      <ChannelInfo></ChannelInfo>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/api/streams" exact>
+          {/* 게시물 리스트를 보여주는 뷰 */}
+          <Channels />
+        </Route>
+        <Route path="/api/streams/:streamId">
+          {/* 특정 게시물의 상세 내용을 보여주는 뷰 */}
+          <Channel />
+        </Route>
+        {/* 다른 라우트 설정 */}
+      </Routes>
+    </Router>
   );
 };
 export default App;
--- src/sbltest/ChannelList.js
+++ src/sbltest/ChannelList.js
@@ -1,17 +1,13 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
 
-const Channel = () => {
-  const { channelId } = useParams();
+const Channels = () => {
   const [channelData, setChannelData] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(
-          "http://localhost:8081/api/channel/${channelId}"
-        );
+        const response = await axios.get("http://localhost:8081/api/channels");
         setChannelData(response.data);
       } catch (error) {
         console.error("Error fetching data:", error);
@@ -19,16 +15,17 @@ const Channel = () => {
     };
 
     fetchData();
-  }, [channelId]);
+  }, []);
 
   return (
     <div>
       <ul>
         {channelData.map((channel) => (
           <li key={channel.channelId}>
+            {/* 여기에 채널 데이터를 표시하는 UI 코드 추가 */}
             {channel.streamerNickname}
             {channel.title}
-            {channel.hlsUrl}
+            {channel.thumbnailUrl}
           </li>
         ))}
       </ul>
@@ -36,4 +33,4 @@ const Channel = () => {
   );
 };
 
-export default Channel;
+export default Channels;

0ab5e5b58a7e7c14b040380c73f1f8b08a326ac5
munee <121735319+alaneelee@users.noreply.github.com>
2023-12-20 22:40:32 +0900

    배포테스트

--- src/components/Slide.js
+++ src/components/Slide.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { StackedCarouselSlideProps } from 'react-stacked-center-carousel';
 import './Slide.css';
 
 export const Slide = React.memo(function (StackedCarouselSlideProps) {

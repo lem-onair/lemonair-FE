89feb8a4baef1adebede7f9f8a7625ac3da01ad3
kangminbeom <alsqja6945@naver.com>
2023-12-28 00:25:18 +0900

    결제기능

--- src/components/payments.js
+++ src/components/payments.js
@@ -7,7 +7,6 @@ const Payment = () => {
     try {
       const response = await axios.post('http://localhost:8081/api/point', {
         point : amount,
-        // Add other necessary data here to send to the server
       }, 
       {
         headers: {
@@ -16,14 +15,11 @@ const Payment = () => {
     });
 
       if (response.status === 200) {
-        // 서버에서 응답을 성공적으로 받았을 때의 작업
         alert('서버로 amount 전송 완료');
       } else {
-        // 서버에서 응답을 받지 못했거나 오류가 있을 때의 작업
         alert('서버로 amount 전송 실패');
       }
     } catch (error) {
-      // 오류가 발생했을 때의 작업
       console.error('서버로 amount 전송 중 오류:', error);
       alert('서버로 amount 전송 중 오류 발생');
     }
@@ -48,15 +44,12 @@ const Payment = () => {
       try {
         const { data } = await axios.post('http://localhost:8081/verifyIamport/' + rsp.imp_uid);
         if (rsp.paid_amount === data.response.amount) {
-          // 결제가 성공한 경우
           alert('결제 성공');
-          sendAmountToServer(rsp.paid_amount); // 결제 성공 후 amount를 서버로 전송
+          sendAmountToServer(rsp.paid_amount);
         } else {
-          // 결제 실패한 경우
           alert('결제 실패');
         }
       } catch (error) {
-        // 결제 검증 중 오류가 발생한 경우
         console.error('결제 검증 중 오류:', error);
         alert('결제 실패');
       }

64d15112c98932e75d687e486f0094541870949d
munee <121735319+alaneelee@users.noreply.github.com>
2023-12-28 13:54:31 +0900

    chat UI

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,46 +1,103 @@
-import React, { useEffect, useState } from "react";
-import { useRef } from "react";
+import React, { useEffect, useState, useRef } from 'react';
+import styled from 'styled-components';
+
+const ChatContainer = styled.div`
+  border: 1px solid #ccc;
+  width: 99.7%;
+  height: 100%;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+`;
+
+const MessageContainer = styled.div`
+  margin-bottom: 10px;
+`;
+
+const MessageBox = styled.div`
+  overflow-y: auto;
+`;
+
+const Sender = styled.span`
+  color: #c0c0c0;
+  font-size: 1rem;
+  padding-left: 5px;
+  font-family: 'Gamja Flower', sans-serif;
+`;
+
+const SpanText = styled.span`
+  font-size: 1.1rem;
+  color: #666;
+  font-family: 'Gamja Flower', sans-serif;
+`;
+
+const InputContainer = styled.div`
+  margin-top: auto;
+  border-top: 1px solid #333333;
+  height: 8vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const TextInput = styled.input`
+  border-radius: 10px;
+  width: 70%;
+  height: 10%;
+  padding: 10px;
+  margin-right: 5px;
+`;
+
+const SendButton = styled.button`
+  font-family: 'Gamja Flower', sans-serif;
+  border-radius: 10px;
+  width: 20%;
+  height: 50%;
+  padding: 5px 10px;
+  background-color: #f8de7e;
+  color: #666;
+  border: none;
+  cursor: pointer;
+`;
 
 const ChatComponent = ({ chatToken, chattingRoomId }) => {
   const [messages, setMessages] = useState([]);
-  const [inputMessage, setInputMessage] = useState("");
+  const [inputMessage, setInputMessage] = useState('');
   const [socket, setSocket] = useState(null);
   const tokenString = chatToken;
+  const messagesEndRef = useRef(null);
 
   useEffect(() => {
     // window.addEventListener("beforeunload", () => {
     //   socket.close();
     // });
-    console.log("chatToken", chatToken);
-    console.log("chattingRommId", chattingRoomId);
+
     const chattingRoomIdString = chattingRoomId;
-    const accessToken = localStorage.getItem("accessToken");
-    console.log(
-      `ws://localhost:8082/chat/${chattingRoomIdString}/${tokenString}`
-    );
+    const accessToken = localStorage.getItem('accessToken');
+
     const newSocket = new WebSocket(
       `ws://localhost:8082/chat/${chattingRoomIdString}/${tokenString}`
     );
     setSocket(newSocket);
     newSocket.onopen = () => {
-      console.log("웹소켓 연결됨");
+      console.log('웹소켓 연결됨');
     };
 
     newSocket.onmessage = (event) => {
       console.log(event.data);
-      const receiveData = event.data.split(":");
+      const receiveData = event.data.split(':');
       const from = receiveData[0];
-      const message = receiveData.slice(1).join(":").trim();
+      const message = receiveData.slice(1).join(':').trim();
       setMessages((prevMessages) => [...prevMessages, { from, message }]);
     };
 
     newSocket.onclose = () => {
-      console.log("웹소켓 연결 종료");
+      console.log('웹소켓 연결 종료');
       // 연결 종료 시 재연결 시도
       setTimeout(async () => {
-        console.log("accessToken", accessToken);
-        const response = await fetch("http://localhost:8081/api/auth/chat", {
-          method: "POST",
+        console.log('accessToken', accessToken);
+        const response = await fetch('http://localhost:8081/api/auth/chat', {
+          method: 'POST',
           headers: {
             Authorization: accessToken,
           },
@@ -52,15 +109,15 @@ const ChatComponent = ({ chatToken, chattingRoomId }) => {
           `ws://localhost:8082/chat/${chattingRoomIdString}/${tokenString}`
         );
         reconnectSocket.onopen = () => {
-          console.log("웹소켓 재연결됨");
+          console.log('웹소켓 재연결됨');
           setSocket(reconnectSocket);
         };
 
         reconnectSocket.onmessage = (event) => {
           console.log(event.data);
-          const receiveData = event.data.split(":");
+          const receiveData = event.data.split(':');
           const from = receiveData[0];
-          const message = receiveData.slice(1).join(":").trim();
+          const message = receiveData.slice(1).join(':').trim();
           setMessages((prevMessages) => [...prevMessages, { from, message }]);
         };
       }, 6000); // 6초 후 재연결 시도
@@ -72,31 +129,40 @@ const ChatComponent = ({ chatToken, chattingRoomId }) => {
   }, []);
 
   const sendMessage = () => {
-    if (inputMessage.trim() === "") return;
+    if (inputMessage.trim() === '') return;
 
     socket.send(inputMessage);
-    setInputMessage("");
+    setInputMessage('');
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      sendMessage();
+    }
   };
 
   return (
-    <div>
-      <div>
+    <ChatContainer>
+      <MessageBox>
         {messages.map((message, index) => (
-          <div key={index}>
-            <strong>{message.from}: </strong>
-            <span>{message.message}</span>
-          </div>
+          <MessageContainer key={index}>
+            <Sender>{message.from}: </Sender>
+            <SpanText>{message.message}</SpanText>
+          </MessageContainer>
         ))}
-      </div>
-      <div>
-        <input
-          type="text"
+        <div ref={messagesEndRef} />
+      </MessageBox>
+      <InputContainer>
+        <TextInput
+          type='text'
           value={inputMessage}
           onChange={(e) => setInputMessage(e.target.value)}
+          onKeyDown={handleKeyDown}
         />
-        <button onClick={sendMessage}>전송</button>
-      </div>
-    </div>
+        <SendButton onClick={sendMessage}>전송</SendButton>
+      </InputContainer>
+    </ChatContainer>
   );
 };
 
--- src/pages/ChannelDetail.js
+++ src/pages/ChannelDetail.js
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import Header from "../components/header";
-import VideoPlayer from "../components/ReactPlayer";
-import styled from "styled-components";
-import Chat from "../components/Chat";
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import Header from '../components/header';
+import VideoPlayer from '../components/ReactPlayer';
+import styled from 'styled-components';
+import Chat from '../components/Chat';
 
 const StreamingContainer = styled.div`
   width: 100%;
@@ -20,12 +20,12 @@ const StreamingTitle = styled.p`
   margin-top: 15px;
   margin-left: 15px;
   font-size: 1.5rem;
-  font-family: "Gamja Flower", sans-serif;
+  font-family: 'Gamja Flower', sans-serif;
 `;
 
 const ChatWrapper = styled.div`
   width: 32%;
-  background-color: forestgreen;
+  background-color: #fff;
 `;
 
 const ChannelDetail = () => {
@@ -39,15 +39,15 @@ const ChannelDetail = () => {
           `http://localhost:8081/api/channels/${id}`
         );
         if (!response.ok) {
-          throw new Error("Network response was not ok.");
+          throw new Error('Network response was not ok.');
         }
         const data = await response.json();
         setChannelData(data);
-        console.log("title:", data.title);
-        console.log("url:", data.hlsUrl);
-        console.log("roomid:", data.chattingRoomId);
+        console.log('title:', data.title);
+        console.log('url:', data.hlsUrl);
+        console.log('roomid:', data.chattingRoomId);
       } catch (error) {
-        console.error("데이터를 불러오는 중 오류가 발생했습니다:", error);
+        console.error('데이터를 불러오는 중 오류가 발생했습니다:', error);
       }
     };
 
@@ -60,10 +60,12 @@ const ChannelDetail = () => {
         <StreamingContainer>
           {channelData ? <VideoPlayer videoUrl={channelData.hlsUrl} /> : null}
           {channelData ? (
-            <Chat
-              chatToken={channelData.chatToken}
-              chattingRoomId={channelData.chattingRoomId}
-            ></Chat>
+            <ChatWrapper>
+              <Chat
+                chatToken={channelData.chatToken}
+                chattingRoomId={channelData.chattingRoomId}
+              ></Chat>
+            </ChatWrapper>
           ) : null}
         </StreamingContainer>
       </StreamingChatContainer>

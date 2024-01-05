a2080fa7fc59e122bf47cf81c7ada18d0c55a7bf
BYEONGRYEOL <lyulbyung@gmail.com>
2024-01-05 18:09:01 +0900

    마지막 재생시간으로 이동하기 개발중

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState, useRef, useCallback } from 'react';
-import styled from 'styled-components';
+import React, { useEffect, useState, useRef, useCallback } from "react";
+import styled from "styled-components";
 
 const ChatContainer = styled.div`
   border: 1px solid #ccc;
@@ -23,13 +23,13 @@ const Sender = styled.span`
   color: #c0c0c0;
   font-size: 1rem;
   padding-left: 5px;
-  font-family: 'Gamja Flower', sans-serif;
+  font-family: "Gamja Flower", sans-serif;
 `;
 
 const SpanText = styled.span`
   font-size: 1.1rem;
   color: #666;
-  font-family: 'Gamja Flower', sans-serif;
+  font-family: "Gamja Flower", sans-serif;
 `;
 
 const InputContainer = styled.div`
@@ -50,7 +50,7 @@ const TextInput = styled.input`
 `;
 
 const SendButton = styled.button`
-  font-family: 'Gamja Flower', sans-serif;
+  font-family: "Gamja Flower", sans-serif;
   border-radius: 10px;
   width: 20%;
   height: 50%;
@@ -63,65 +63,73 @@ const SendButton = styled.button`
 
 const ChatComponent = ({ chattingRoomId }) => {
   const [messages, setMessages] = useState([]);
-  const [inputMessage, setInputMessage] = useState('');
+  const [inputMessage, setInputMessage] = useState("");
   const [socket, setSocket] = useState(null);
+  const [socketIntervalId, setSocketIntervalId] = useState(null);
   const messagesEndRef = useRef(null);
 
   const chattingRoomIdString = chattingRoomId;
-  const accessToken = localStorage.getItem('accessToken');
+  const accessToken = localStorage.getItem("accessToken");
   const fetchToken = useCallback(async () => {
     try {
       console.log(accessToken);
-      const response = await fetch('https://api.lemonair.me/api/auth/chat', {
-        method: 'POST',
+      const response = await fetch("https://api.lemonair.me/api/auth/chat", {
+        method: "POST",
         headers: {
           Authorization: accessToken,
         },
       });
       if (!response.ok) {
-        throw new Error('Network response was not ok.');
+        throw new Error("Network response was not ok.");
       }
       const data = await response.json();
-      console.log('서버에서 받은 chatTokenResponseDto : ', data);
+      console.log("서버에서 받은 chatTokenResponseDto : ", data);
       return data.chatToken;
     } catch (error) {
-      console.error('데이터를 불러오는 중 오류가 발생했습니다:', error);
+      console.error("데이터를 불러오는 중 오류가 발생했습니다:", error);
     }
   }, [accessToken]);
 
   useEffect(() => {
     const connectWebSocketAsync = async () => {
-      let chatToken = '';
-      console.log('accessToken is null ? :', accessToken === null);
+      let chatToken = "";
+      console.log("accessToken is null ? :", accessToken === null);
       if (accessToken) {
         chatToken = await fetchToken();
       } else {
-        chatToken = 'notlogin'; // 로그인하지 않은 사용자의 경우 토큰 정보를 notlogin으로 요청한다.
+        chatToken = "notlogin"; // 로그인하지 않은 사용자의 경우 토큰 정보를 notlogin으로 요청한다.
       }
       const newSocket = new WebSocket(
         `wss://chat.lemonair.me/chat/${chattingRoomIdString}/${chatToken}`
       );
       setSocket(newSocket);
       newSocket.onopen = () => {
-        console.log('웹소켓 연결됨');
+        console.log("웹소켓 연결됨");
+        const heartbeatInterval = setInterval(() => {
+          newSocket.send("heartbeat");
+        }, 30000);
+
+        setSocketIntervalId(heartbeatInterval);
       };
 
       newSocket.onmessage = (event) => {
         console.log(event.data);
-        const receiveData = event.data.split(':');
+        const receiveData = event.data.split(":");
         const from = receiveData[0];
-        const message = receiveData.slice(1).join(':').trim();
+        const message = receiveData.slice(1).join(":").trim();
         setMessages((prevMessages) => [...prevMessages, { from, message }]);
       };
 
       newSocket.onclose = () => {
-        console.log('웹소켓 연결 종료');
+        console.log("웹소켓 연결 종료");
+        clearInterval(socketIntervalId);
+
         // 연결 종료 시 재연결 시도
         setTimeout(async () => {
           if (accessToken) {
             chatToken = await fetchToken();
           } else {
-            chatToken = 'notlogin'; // 로그인하지 않은 사용자의 경우 토큰 정보를 notlogin으로 요청한다.
+            chatToken = "notlogin"; // 로그인하지 않은 사용자의 경우 토큰 정보를 notlogin으로 요청한다.
           }
           const reconnectSocket = new WebSocket(
             `wss://chat.lemonair.me/chat/${chattingRoomIdString}/${chatToken}`
@@ -129,21 +137,26 @@ const ChatComponent = ({ chattingRoomId }) => {
           setSocket(reconnectSocket);
 
           reconnectSocket.onopen = () => {
-            console.log('웹소켓 재연결됨');
+            console.log("웹소켓 재연결됨");
             setSocket(reconnectSocket);
+            const heartbeatInterval = setInterval(() => {
+              newSocket.send("heartbeat");
+            }, 30000);
+            setSocketIntervalId(heartbeatInterval);
           };
 
           reconnectSocket.onmessage = (event) => {
             console.log(event.data);
-            const receiveData = event.data.split(':');
+            const receiveData = event.data.split(":");
             const from = receiveData[0];
-            const message = receiveData.slice(1).join(':').trim();
+            const message = receiveData.slice(1).join(":").trim();
             setMessages((prevMessages) => [...prevMessages, { from, message }]);
           };
         }, 1000); // 1초 후 재연결 시도
       };
       return () => {
         newSocket.close();
+        clearInterval(socketIntervalId);
       };
     };
 
@@ -151,20 +164,20 @@ const ChatComponent = ({ chattingRoomId }) => {
   }, [accessToken, chattingRoomIdString, fetchToken]);
 
   const scrollToBottom = () => {
-    messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
   };
 
   useEffect(scrollToBottom, [messages]);
 
   const sendMessage = () => {
-    if (inputMessage.trim() === '') return;
+    if (inputMessage.trim() === "") return;
 
     socket.send(inputMessage);
-    setInputMessage('');
+    setInputMessage("");
   };
 
   const handleKeyDown = (event) => {
-    if (event.key === 'Enter') {
+    if (event.key === "Enter") {
       event.preventDefault();
       sendMessage();
     }
@@ -183,7 +196,7 @@ const ChatComponent = ({ chattingRoomId }) => {
       </MessageBox>
       <InputContainer>
         <TextInput
-          type='text'
+          type="text"
           value={inputMessage}
           onChange={(e) => setInputMessage(e.target.value)}
           onKeyDown={handleKeyDown}
--- src/components/HlsPlayer.js
+++ src/components/HlsPlayer.js
@@ -11,160 +11,167 @@ const HlsVideoPlayer = ({ videoUrl }) => {
     console.log("useEffect실행");
     const initializeHls = (data) => {
       console.log(data);
-      console.log("initialize 실행");
-      // 자동재생
-      console.log("play 실행 전");
       videoElement.play();
+      // videoElement.muted = false;
+      // videoElement.play();
       console.log("play 실행 후ㅡ");
       // 마지막 청크의 재생시간으로 이동
       console.log("hls", hls);
       // console.log("hls.media.current.segments", hls.media.current.segments);
-      console.log(data.lastSegment);
-      console.log(hls.media.segments);
-      const lastSegment = hls.media.segments[hls.media.segments.length - 1];
+      console.log("data : ", data);
+      let myFragments = data.levels[0].details.fragments;
+      // console.log(hls.media.segments);
+      const lastSegment = myFragments[myFragments.length - 1];
       console.log("lastSegment", lastSegment);
       if (lastSegment) {
-        videoElement.currentTime = lastSegment.end;
+        videoElement.currentTime = lastSegment.start - 10;
       }
     };
+    document.addEventListener("DOMContentLoaded", function () {
+      setTimeout(function () {
+        videoElement.muted = false;
+      }, 1000); // 예: 1초 후에 재생
+    });
 
     // Hls 지원 여부 확인
-    if (Hls.isSupported()) {
-      console.log("hls를 지원한다.");
-      var config = {
-        autoStartLoad: true,
-        startPosition: -1,
-        debug: false,
-        capLevelOnFPSDrop: false,
-        capLevelToPlayerSize: false,
-        defaultAudioCodec: undefined,
-        initialLiveManifestSize: 1,
-        maxBufferLength: 30,
-        maxMaxBufferLength: 600,
-        backBufferLength: Infinity,
-        frontBufferFlushThreshold: Infinity,
-        maxBufferSize: 60 * 1000 * 1000,
-        maxBufferHole: 0.5,
-        highBufferWatchdogPeriod: 2,
-        nudgeOffset: 0.1,
-        nudgeMaxRetry: 3,
-        maxFragLookUpTolerance: 0.25,
-        liveSyncDurationCount: 3,
-        liveMaxLatencyDurationCount: Infinity,
-        liveDurationInfinity: false,
-        preferManagedMediaSource: false,
-        enableWorker: true,
-        enableSoftwareAES: true,
-        fragLoadPolicy: {
-          default: {
-            maxTimeToFirstByteMs: 9000,
-            maxLoadTimeMs: 100000,
-            timeoutRetry: {
-              maxNumRetry: 2,
-              retryDelayMs: 0,
-              maxRetryDelayMs: 0,
-            },
-            errorRetry: {
-              maxNumRetry: 5,
-              retryDelayMs: 3000,
-              maxRetryDelayMs: 15000,
-              backoff: "linear",
+    const loadVideo = async () => {
+      if (Hls.isSupported()) {
+        console.log("hls를 지원한다.");
+        var config = {
+          autoStartLoad: true,
+          startPosition: -1,
+          debug: false,
+          capLevelOnFPSDrop: false,
+          capLevelToPlayerSize: false,
+          defaultAudioCodec: undefined,
+          initialLiveManifestSize: 1,
+          maxBufferLength: 30,
+          maxMaxBufferLength: 600,
+          backBufferLength: Infinity,
+          frontBufferFlushThreshold: Infinity,
+          maxBufferSize: 60 * 1000 * 1000,
+          maxBufferHole: 0.5,
+          highBufferWatchdogPeriod: 2,
+          nudgeOffset: 0.1,
+          nudgeMaxRetry: 3,
+          maxFragLookUpTolerance: 0.25,
+          liveSyncDurationCount: 3,
+          liveMaxLatencyDurationCount: Infinity,
+          liveDurationInfinity: false,
+          preferManagedMediaSource: false,
+          enableWorker: true,
+          enableSoftwareAES: true,
+          fragLoadPolicy: {
+            default: {
+              maxTimeToFirstByteMs: 9000,
+              maxLoadTimeMs: 100000,
+              timeoutRetry: {
+                maxNumRetry: 2,
+                retryDelayMs: 0,
+                maxRetryDelayMs: 0,
+              },
+              errorRetry: {
+                maxNumRetry: 5,
+                retryDelayMs: 3000,
+                maxRetryDelayMs: 15000,
+                backoff: "linear",
+              },
             },
           },
-        },
-        startLevel: undefined,
-        audioPreference: {
-          characteristics: "public.accessibility.describes-video",
-        },
-        subtitlePreference: {
-          lang: "en-US",
-        },
-        startFragPrefetch: false,
-        testBandwidth: true,
-        progressive: false,
-        lowLatencyMode: true,
-        fpsDroppedMonitoringPeriod: 5000,
-        fpsDroppedMonitoringThreshold: 0.2,
-        appendErrorMaxRetry: 3,
-        // loader: customLoader,
-        // fLoader: customFragmentLoader,
-        // pLoader: customPlaylistLoader,
-        // xhrSetup: XMLHttpRequestSetupCallback,
-        // fetchSetup: FetchSetupCallback,
-        // abrController: AbrController,
-        // bufferController: BufferController,
-        // capLevelController: CapLevelController,
-        // fpsController: FPSController,
-        // timelineController: TimelineController,
-        enableDateRangeMetadataCues: true,
-        enableEmsgMetadataCues: true,
-        enableID3MetadataCues: true,
-        enableWebVTT: true,
-        enableIMSC1: true,
-        enableCEA708Captions: true,
-        stretchShortVideoTrack: false,
-        maxAudioFramesDrift: 1,
-        forceKeyFrameOnDiscontinuity: true,
-        abrEwmaFastLive: 3.0,
-        abrEwmaSlowLive: 9.0,
-        abrEwmaFastVoD: 3.0,
-        abrEwmaSlowVoD: 9.0,
-        abrEwmaDefaultEstimate: 500000,
-        abrEwmaDefaultEstimateMax: 5000000,
-        abrBandWidthFactor: 0.95,
-        abrBandWidthUpFactor: 0.7,
-        abrMaxWithRealBitrate: false,
-        maxStarvationDelay: 4,
-        maxLoadingDelay: 4,
-        minAutoBitrate: 0,
-        emeEnabled: false,
-        licenseXhrSetup: undefined,
-        drmSystems: {},
-        drmSystemOptions: {},
-        // requestMediaKeySystemAccessFunc: requestMediaKeySystemAccess,
-        cmcd: {
-          // sessionId: uuid(),
-          // contentId: hash(contentURL),
-          useHeaders: false,
-        },
-      };
+          startLevel: undefined,
+          audioPreference: {
+            characteristics: "public.accessibility.describes-video",
+          },
+          subtitlePreference: {
+            lang: "en-US",
+          },
+          startFragPrefetch: false,
+          testBandwidth: true,
+          progressive: false,
+          lowLatencyMode: true,
+          fpsDroppedMonitoringPeriod: 5000,
+          fpsDroppedMonitoringThreshold: 0.2,
+          appendErrorMaxRetry: 3,
+          // loader: customLoader,
+          // fLoader: customFragmentLoader,
+          // pLoader: customPlaylistLoader,
+          // xhrSetup: XMLHttpRequestSetupCallback,
+          // fetchSetup: FetchSetupCallback,
+          // abrController: AbrController,
+          // bufferController: BufferController,
+          // capLevelController: CapLevelController,
+          // fpsController: FPSController,
+          // timelineController: TimelineController,
+          enableDateRangeMetadataCues: true,
+          enableEmsgMetadataCues: true,
+          enableID3MetadataCues: true,
+          enableWebVTT: true,
+          enableIMSC1: true,
+          enableCEA708Captions: true,
+          stretchShortVideoTrack: false,
+          maxAudioFramesDrift: 1,
+          forceKeyFrameOnDiscontinuity: true,
+          abrEwmaFastLive: 3.0,
+          abrEwmaSlowLive: 9.0,
+          abrEwmaFastVoD: 3.0,
+          abrEwmaSlowVoD: 9.0,
+          abrEwmaDefaultEstimate: 500000,
+          abrEwmaDefaultEstimateMax: 5000000,
+          abrBandWidthFactor: 0.95,
+          abrBandWidthUpFactor: 0.7,
+          abrMaxWithRealBitrate: false,
+          maxStarvationDelay: 4,
+          maxLoadingDelay: 4,
+          minAutoBitrate: 0,
+          emeEnabled: false,
+          licenseXhrSetup: undefined,
+          drmSystems: {},
+          drmSystemOptions: {},
+          // requestMediaKeySystemAccessFunc: requestMediaKeySystemAccess,
+          cmcd: {
+            // sessionId: uuid(),
+            // contentId: hash(contentURL),
+            useHeaders: false,
+          },
+        };
 
-      hls = new Hls(config);
-      // 이벤트 리스너 등록
-      // hls.on(Hls.Events.MEDIA_ATTACHED, (event, data) => {
-      //   console.log("event listener 등록");
-      //   initializeHls(data);
-      // });
+        hls = new Hls(config);
+        // 이벤트 리스너 등록
+        // hls.on(Hls.Events.MEDIA_ATTACHED, (event, data) => {
+        //   console.log("event listener 등록");
+        //   initializeHls(data);
+        // });
 
-      hls.on(Hls.Events.MEDIA_ATTACHED, function () {
-        console.log("video and hls.js are now bound together !");
-      });
-      hls.on(Hls.Events.MANIFEST_PARSED, function (event, data) {
-        initializeHls(data);
-      });
+        hls.on(Hls.Events.MEDIA_ATTACHED, function () {
+          console.log("video and hls.js are now bound together !");
+        });
+        hls.on(Hls.Events.MANIFEST_PARSED, function (event, data) {
+          console.log("hls 로드될때 data는?", data);
+          initializeHls(data);
+        });
 
-      hls.loadSource(videoUrl);
-      hls.attachMedia(videoElement);
-    } else if (videoElement.canPlayType("application/vnd.apple.mpegurl")) {
-      // Hls 지원하지 않을 경우
-      videoElement.src = videoUrl;
-      console.log("hls 지원하지 않는 경우");
-      // 자동재생
-      videoElement.play();
-    }
-
-    // 컴포넌트가 언마운트될 때 해제
-    return () => {
-      if (Hls.isSupported()) {
-        console.log("컴포넌트 unmount시에 destroy");
-        hls.destroy();
+        hls.loadSource(videoUrl);
+        hls.attachMedia(videoElement);
+      } else if (videoElement.canPlayType("application/vnd.apple.mpegurl")) {
+        // Hls 지원하지 않을 경우
+        videoElement.src = videoUrl;
+        console.log("hls 지원하지 않는 경우");
+        // 자동재생
+        videoElement.play();
       }
     };
-  }, [videoUrl]);
+
+    loadVideo();
+    // 컴포넌트가 언마운트될 때 해제
+    return () => {
+      console.log("컴포넌트 unmount시에 destroy");
+      hls.destroy();
+    };
+  }, []);
 
   return (
     <div>
-      <video ref={videoRef} controls width="100%" height="100%" />
+      <video ref={videoRef} controls width="100%" height="100%" muted />
     </div>
   );
 };
